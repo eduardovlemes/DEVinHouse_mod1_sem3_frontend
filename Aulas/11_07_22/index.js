affa569e6be8c13def3b8b626d46dce3f707878d
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-07-12 18:17:58 -0300

    fix: ajustando arquivo

--- Aulas/11_07_22/index.js
+++ Aulas/11_07_22/index.js
@@ -1,12 +1,36 @@
-
-//function cumprimentar(){
-//    console.log('oi')
-//}
+function cumprimentar(){
+    console.log('oi')
+}
 cumprimentar()
+
 
 
 function soma(){
     console.log(a + b)
 }
-
 soma(1,2)
+
+
+
+// Exercício 1 - alunos
+var informarParImpar = function (num1){
+    if(num1 % 2 == 0){
+        return 'Par'
+    } else if (num1 %2 == 1){
+        return 'Ímpar'
+    } else {
+        return 'Invalido'
+    }
+}
+console.log(informarParImpar())
+
+        
+// Exercício 2 -  alunos 
+function imprimeElementosDaLista (lista = []){    
+    for (let item of lista){
+        console.log (item);
+    }
+    let quantidade = "A quantidade de elementos da lista são " + lista.length;
+    return quantidade
+}
+console.log (imprimeElementosDaLista(["oi", "turu", "bom"]));
