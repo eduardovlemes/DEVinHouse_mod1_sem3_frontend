c6027ab8c5c63bd082c2a399a78a80021d8a2919
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-07-12 18:38:30 -0300

    feat: ajustes

--- Aulas/11_07_22/index.js
+++ Aulas/11_07_22/index.js
@@ -1,36 +1,33 @@
-function cumprimentar(){
-    console.log('oi')
+function cumprimentar() {
+  console.log("oi");
 }
-cumprimentar()
+cumprimentar();
 
-
-
-function soma(){
-    console.log(a + b)
+function soma() {
+  console.log(a + b);
 }
-soma(1,2)
-
-
+soma(1, 2);
 
 // Exercício 1 - alunos
-var informarParImpar = function (num1){
-    if(num1 % 2 == 0){
-        return 'Par'
-    } else if (num1 %2 == 1){
-        return 'Ímpar'
-    } else {
-        return 'Invalido'
-    }
-}
-console.log(informarParImpar())
+var informarParImpar = function (num1) {
+  if (num1 % 2 == 0) {
+    return "Par";
+  } else if (num1 % 2 == 1) {
+    return "Ímpar";
+  } else {
+    return "Invalido";
+  }
+};
+console.log(informarParImpar());
 
-        
-// Exercício 2 -  alunos 
-function imprimeElementosDaLista (lista = []){    
-    for (let item of lista){
-        console.log (item);
-    }
-    let quantidade = "A quantidade de elementos da lista são " + lista.length;
-    return quantidade
+
+
+// Exercício 2 -  alunos
+function imprimeElementosDaLista(lista = []) {
+  for (let item of lista) {
+    console.log(item);
+  }
+  let quantidade = "A quantidade de elementos da lista são " + lista.length;
+  return quantidade;
 }
-console.log (imprimeElementosDaLista(["oi", "turu", "bom"]));
+console.log(imprimeElementosDaLista(["oi", "turu", "bom"]));
