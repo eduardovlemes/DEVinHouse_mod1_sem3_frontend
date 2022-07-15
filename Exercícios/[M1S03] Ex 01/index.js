c8f14ebad499c638af6deadebf78c0655886eb1c
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-07-15 10:41:38 -0300

    feat: ajuste do arquivo

--- Exercícios/[M1S03] Ex 01/index.js
+++ Exercícios/[M1S03] Ex 01/index.js
@@ -1,3 +1,5 @@
+//Crie uma função não nomeada que recebe um número e devolva uma string dizendo se é par ou ímpar.
+
 var descobrirSeEParOuImpar = function (numero) {
     if (numero % 2 == 0) {
       return "Par"
