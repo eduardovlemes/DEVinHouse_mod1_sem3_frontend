c98ee4dd22429a2f0abd36e4456ce00e42387970
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-07-17 18:40:37 -0300

    feat: ajustes

--- Exercícios/[M1S03]_Ex_06/index.js
+++ Exercícios/[M1S03]_Ex_06/index.js
@@ -1,6 +1,6 @@
 //Faça um site com uma lista de itens (por exemplo, de brinquedos), que tenha: um campo de input do item um botão para criar um item na lista
-function adicionar(){
-    const input = document.getElementById('meu-input')
-    const divPai = document.getElementById('divPai')
-    divPai.innerHTML=(input.value)
+function adicionar () {
+    const input = document.getElementById("meu-input");
+    const divPai = document.getElementById("pai");
+    divPai.innerHTML = (input.value);
 }
